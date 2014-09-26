367f61a176453939864e4a748d385ec5ccb9f361
ikoddn <ikonage@gmail.com>
2014-09-26 13:54:18 +0200

    Song and User classes implementation.

---
+++ INF5040-Obl1/src/no/uio/inf5040/obl1/server/SongImpl.java
@@ -0,0 +1,9 @@
+package no.uio.inf5040.obl1.server;
+
+import no.uio.inf5040.obl1.tasteprofile.Song;
+
+public class SongImpl extends Song {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ INF5040-Obl1/src/no/uio/inf5040/obl1/server/UserImpl.java
@@ -0,0 +1,9 @@
+package no.uio.inf5040.obl1.server;
+
+import no.uio.inf5040.obl1.tasteprofile.User;
+
+public class UserImpl extends User {
+
+	private static final long serialVersionUID = 1L;
+
+}
