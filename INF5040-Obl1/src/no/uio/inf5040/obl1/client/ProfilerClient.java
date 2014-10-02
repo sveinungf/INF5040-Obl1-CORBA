8c1f9a4c0ad1b67f6d479d6110445eb8534277c4
ikoddn <ikonage@gmail.com>
2014-10-02 15:19:58 +0200

    Client: Reverted the "old" ProfilerClient

--- INF5040-Obl1/src/no/uio/inf5040/obl1/client/ProfilerClient.java
+++ INF5040-Obl1/src/no/uio/inf5040/obl1/client/ProfilerClient.java
@@ -1,13 +1,7 @@
 package no.uio.inf5040.obl1.client;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import no.uio.inf5040.obl1.tasteprofile.Profiler;
 import no.uio.inf5040.obl1.tasteprofile.ProfilerHelper;
-import no.uio.inf5040.obl1.tasteprofile.Song;
-import no.uio.inf5040.obl1.tasteprofile.User;
-import no.uio.inf5040.obl1.tasteprofile.UserHolder;
 
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NamingContextExt;
@@ -15,81 +9,42 @@ import org.omg.CosNaming.NamingContextExtHelper;
 
 public class ProfilerClient {
 
-	private boolean cacheUsers;
-	private Map<String, User> cache;
-	private Profiler profiler;
-
-	public ProfilerClient(Profiler profiler, boolean cacheUsers) {
-		this.cacheUsers = cacheUsers;
-		cache = new HashMap<String, User>();
-		this.profiler = profiler;
-	}
-
-	public String getOutput(String method, String arg1, String arg2) {
-		String userId, songId;
-		StringBuilder sb = new StringBuilder();
-		int timesPlayed = 0;
-		long before = System.nanoTime();
-		long after;
-
-		switch (method) {
-		case "getTimesPlayed":
-			songId = arg1;
-
-			timesPlayed = profiler.getTimesPlayed(songId);
-			after = System.nanoTime();
-
-			sb.append("Song ");
-			sb.append(songId);
-			sb.append(" played ");
-			sb.append(timesPlayed);
-			sb.append(" times. (");
-			sb.append((after - before) / 1000);
-			sb.append(" ms)");
-			break;
-		case "getTimesPlayedByUser":
-			userId = arg1;
-			songId = arg2;
-
-			if (cacheUsers) {
-				User user = cache.get(userId);
-
-				if (user == null) {
-					UserHolder userHolder = new UserHolder();
-					timesPlayed = profiler.getUserProfile(userId, songId,
-							userHolder);
-
-					cache.put(userId, userHolder.value);
-				} else {
-					for (Song song : user.songs) {
-						if (song.id.equals(songId)) {
-							timesPlayed = song.play_count;
-							break;
-						}
-					}
-				}
-			} else {
-				timesPlayed = profiler.getTimesPlayedByUser(userId, songId);
-			}
-
-			after = System.nanoTime();
-
-			sb.append("Song ");
-			sb.append(songId);
-			sb.append(" played ");
-			sb.append(timesPlayed);
-			sb.append(" times by user ");
-			sb.append(userId);
-			sb.append(". (");
-			sb.append((after - before) / 1000);
-			sb.append(" ms)");
-			break;
-		}
-
-		return sb.toString();
-	}
+	private static final String ARG_CACHE = "-cache";
+	private static final String ARG_INPUT = "-in";
+	private static final String ARG_OUTPUT = "-out";
 
 	public static void main(String[] args) {
+		boolean cacheUsers = false;
+		String inputfile = null;
+		String outputfile = "output.txt";
+
+		for (int i = 0; i < args.length; ++i) {
+			switch (args[i]) {
+			case ARG_CACHE:
+				cacheUsers = true;
+				break;
+			case ARG_INPUT:
+				++i;
+				inputfile = args[i];
+				break;
+			case ARG_OUTPUT:
+				++i;
+				outputfile = args[i];
+				break;
+			}
+		}
+
+		if (inputfile == null) {
+			System.out.println("Error: Must supply input file.");
+			System.out.println("Usage: this -ORBInitialPort <port>"
+					+ " -ORBInitialHost <host> " + ARG_INPUT
+					+ " <path to input file> [" + ARG_CACHE + " " + ARG_OUTPUT
+					+ " <path to output file>]");
+			return;
+		}
+
+		System.out.println("Starting client...");
+
 		ORB orb = ORB.init(args, null);
 
 		try {
@@ -100,17 +55,15 @@ public class ProfilerClient {
 			String name = "Profiler";
 			Profiler profiler = ProfilerHelper.narrow(ncRef.resolve_str(name));
 
-			boolean cacheUsers = false;
-			ProfilerClient client = new ProfilerClient(profiler, cacheUsers);
+			RemoteInvocator invocator = new RemoteInvocator(profiler,
+					cacheUsers);
 
-			// Read input.txt
-			String method = "getTimesPlayed";
-			String arg1 = "SOFRDND12A58A7D6C5";
-			String arg2 = null;
+			FileHandler infile = new FileHandler(inputfile);
+			FileHandler outfile = new FileHandler(outputfile);
 
-			String output = client.getOutput(method, arg1, arg2);
-			System.out.println(output);
-			// Write to file
+			infile.readTo(invocator);
+
+			outfile.write(invocator.getResults());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
