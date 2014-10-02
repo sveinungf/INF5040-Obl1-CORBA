4f6421b24fea9bcda66bd559216d6978c0602e4f
ikoddn <ikonage@gmail.com>
2014-10-02 14:19:54 +0200

    Client: Caching and output file as command-line arguments

--- INF5040-Obl1/src/no/uio/inf5040/obl1/client/ProfilerClient.java
+++ INF5040-Obl1/src/no/uio/inf5040/obl1/client/ProfilerClient.java
@@ -9,22 +9,37 @@ import org.omg.CosNaming.NamingContextExtHelper;
 
 public class ProfilerClient {
 
-	private static final String INPUT_ARG = "-in";
+	private static final String ARG_CACHE = "-cache";
+	private static final String ARG_INPUT = "-in";
+	private static final String ARG_OUTPUT = "-out";
 
 	public static void main(String[] args) {
+		boolean cacheUsers = false;
 		String inputfile = null;
+		String outputfile = "output.txt";
 
-		for (int i = 0; i < args.length - 1 && inputfile == null; ++i) {
-			if (INPUT_ARG.equals(args[i])) {
-				inputfile = args[i + 1];
+		for (int i = 0; i < args.length; ++i) {
+			switch (args[i]) {
+			case ARG_CACHE:
+				cacheUsers = true;
+				break;
+			case ARG_INPUT:
+				++i;
+				inputfile = args[i];
+				break;
+			case ARG_OUTPUT:
+				++i;
+				outputfile = args[i];
+				break;
 			}
 		}
 
 		if (inputfile == null) {
 			System.out.println("Error: Must supply input file.");
 			System.out.println("Usage: this -ORBInitialPort <port>"
-					+ " -ORBInitialHost <host> " + INPUT_ARG
-					+ " <path to input file>");
+					+ " -ORBInitialHost <host> " + ARG_INPUT
+					+ " <path to input file> [" + ARG_CACHE + " " + ARG_OUTPUT
+					+ " <path to output file>]");
 			return;
 		}
 
@@ -40,12 +55,11 @@ public class ProfilerClient {
 			String name = "Profiler";
 			Profiler profiler = ProfilerHelper.narrow(ncRef.resolve_str(name));
 
-			boolean cacheUsers = false;
 			RemoteInvocator invocator = new RemoteInvocator(profiler,
 					cacheUsers);
 
 			FileHandler infile = new FileHandler(inputfile);
-			FileHandler outfile = new FileHandler("output.txt");
+			FileHandler outfile = new FileHandler(outputfile);
 
 			infile.readTo(invocator);
 
